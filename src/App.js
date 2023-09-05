25280f44054268ccdfb21cc654d8cb64567a9062
JhairTA <jhairtiradoatalaya@gmail.com>
2023-09-04 21:52:40 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import { useState } from 'react';
+import Swal from 'sweetalert2'
 
 function App() {
+
+  const [numClics, setNumClics] = useState(0); 
+
+  const manejarClic = () =>{
+    setNumClics(numClics + 1);
+  };
+
+  const reiniciarContador = () =>{
+    if (numClics !== 0){
+      Swal.fire({
+        title: 'Reset counter?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, restart!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Restarted!',
+            'The counter has been reset.',
+            'success'
+          )
+          setNumClics(0);
+        }
+      })
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor'>
+        <h1>CLICK COUNTER</h1>
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
+        <Boton
+          texto="Click"
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+          
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
+      </div>
     </div>
   );
 }
